b28288c5101b2b0826f91d3330c1f4358852d367
vishaldeshmukh007 <vishaldeshmukh007@users.noreply.github.com>
2016-06-12 00:14:52 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,70 @@
+package com.test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		int iMaxNumber  = 100;
+		int totalNumberOFThreads =4;
+		objectPrinter obeObjectPrinter = new objectPrinter(iMaxNumber, totalNumberOFThreads);
+
+		for(int i = 1 ;i <= totalNumberOFThreads; i++){
+			ThreadObject TH = new ThreadObject(i==totalNumberOFThreads ? 0 : i,obeObjectPrinter);
+			Thread thread = new Thread(TH);
+			thread.start();
+		}
+	}
+
+}
+
+class objectPrinter{
+
+	int iMaxNumber;
+	volatile int i = 1;
+	int TotalNumberOFThreads;
+
+
+	public objectPrinter(int iMaxNumber, int totalNumberOFThreads) {
+		super();
+		this.iMaxNumber = iMaxNumber;
+		TotalNumberOFThreads = totalNumberOFThreads;
+	}
+
+
+	synchronized void printNumber(int iCurrentThread){
+		while(isPrintNumber()){
+			if(i%TotalNumberOFThreads == iCurrentThread){
+				System.out.println("Thread => "+ iCurrentThread  + " | Value=> " + i++);
+				notifyAll();
+			}else{
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	boolean isPrintNumber(){
+		return iMaxNumber >= i;
+	}
+}
+
+class ThreadObject implements Runnable{
+	int iThreadNUmber;
+	objectPrinter objectPrinter;
+
+	public ThreadObject(int iThreadNUmber, com.test.objectPrinter objectPrinter) {
+		super();
+		this.iThreadNUmber = iThreadNUmber;
+		this.objectPrinter = objectPrinter;
+	}
+
+	public void run(){
+	//	while(objectPrinter.isPrintNumber())
+			objectPrinter.printNumber(iThreadNUmber);
+	}
+}
